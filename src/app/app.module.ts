34c15bd0b2a7b0011a132bba834ec54fe60f1bb8
Maciej Krupiński <mkrupinski@software.altkom.pl>
2020-01-16 15:08:46 +0100

    app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent } from './messages/messages.component';
+import { BeerSearchComponent } from './beer-search/beer-search.component';
+import { BeerDetailComponent } from './beer-detail/beer-detail.component';
+import { BeersComponent } from './beers/beers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MessagesComponent,
+    BeerSearchComponent,
+    BeerDetailComponent,
+    BeersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
